c5da81c183cfa1bee8b3fe5a2e1262727d0d6595
jesjacobs <57545607+jesjacobs@users.noreply.github.com>
2024-04-08 18:30:23 -0700

    Update dataset.py

--- XCS224N-A5-master/src/submission/dataset.py
+++ XCS224N-A5-master/src/submission/dataset.py
@@ -176,9 +176,25 @@ class CharCorruptionDataset(Dataset):
         ### [part e]: see spec above
 
         ### START CODE HERE
+        doc = self.data[idx]
+        truncated_len = np.random.randint(4, int(self.block_size * 3/4))
+        doc = doc[:truncated_len]  # Truncate to given length.
+        masked_content_len = truncated_len * np.random.normal(0.25, 0.1)
+        masked_content_len = int(np.clip(masked_content_len, 1, truncated_len - 2))  # Clip invalid random indices.
+        masked_content_idx = np.random.randint(1, truncated_len - masked_content_len)
+        prefix = doc[:masked_content_idx]
+        masked_content = doc[masked_content_idx:masked_content_idx + masked_content_len]
+        suffix = doc[masked_content_idx + masked_content_len:]
+        masked_content = prefix + self.MASK_CHAR + suffix + self.MASK_CHAR + masked_content  # Rearrange.
+        masked_content += self.PAD_CHAR * (self.block_size - len(masked_content))  # Pad to block_size.
+
+        # Encode into long tensors.
+        x = torch.tensor([self.stoi[c] for c in masked_content[:-1]], dtype=torch.long)
+        y = torch.tensor([self.stoi[c] for c in masked_content[1:]], dtype=torch.long)
+        return x, y      
         ### END CODE HERE
 
-        raise NotImplementedError
+        #raise NotImplementedError
 
 """
 Code under here is strictly for your debugging purposes; feel free to modify
